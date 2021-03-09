51eb331ce69fe777872d29ec1f2eb4b8c354bd54
gothello <marcellosanttos2014@gmail.com>
2021-03-09 19:10:06 -0300

    add goroutines and files

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+
+	"fmt"
 	"github.com/gothello/go-scanner/port"
 )
 
-func main() {
-	port.GetOpenPorts("189.52.154.213", port.PortRange{80, 8080})
 
+
+func main() {
+
+	proxy := "169.159.131.146"
+
+	scan := port.ScanPorts(proxy)
+	
+	fmt.Println(proxy)
+	for _, value := range scan {
+		fmt.Printf("%+v\n", value)
+	}
 }
--- port/port.go
+++ port/port.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"net"
 	"time"
+	"sync"
 )
+
+var wg *sync.WaitGroup
 
 type PortResult struct {
 	Port    int
@@ -17,70 +20,41 @@ type PortRange struct {
 	End   int
 }
 
-type ScanResult struct {
-	hostname string
-	ip       []net.IP
-	results  []PortResult
+func ScanPorts(hostname string) []PortResult{
+	var portResult []PortResult
+	results := make(chan PortResult)
+
+	for port, service := range common {
+		
+		wg.Add(1)
+		go ScanPort(hostname, port, service, results, wg)
+	}
+
+	
+
+	for result := range results {
+		portResult = append(portResult, result)
+	}
+	
+	wg.Wait()
+		
+	return portResult
 }
 
-func GetOpenPorts(hostname string, ports PortRange) {
-	scanned, err := ScanPorts(hostname, ports)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		Display(scanned)
-	}
-}
-
-func Display(result ScanResult) {
-	ip := result.ip[len(result.ip)-1]
-
-	fmt.Printf("Open ports for %s (%s)\n", result.hostname, ip.String())
-
-	for _, v := range result.results {
-		if v.State {
-			fmt.Printf("%d %s\n", v.Port, v.Service)
-		}
-	}
-}
-
-func ScanPorts(hostname string, ports PortRange) (ScanResult, error) {
-	var results []PortResult
-	var scanned ScanResult
-	addr, err := net.LookupIP(hostname)
-	if err != nil {
-		return scanned, err
-	}
-
-	for p, s := range common {
-		result := ScanPort(hostname, p)
-		result.Service = s
-
-		results = append(results, result)
-	}
-
-	scanned = ScanResult{
-		hostname: hostname,
-		ip:       addr,
-		results:  results,
-	}
-
-	return scanned, nil
-}
-
-func ScanPort(hostname string, port int) PortResult {
+func ScanPort(hostname string, port int, service string, result chan PortResult, wg *sync.WaitGroup) {
+	defer wg.Done()	
 	address := fmt.Sprintf("%s:%d", hostname, port)
 	portResult := PortResult{
 		Port: port,
+		Service: service,
 	}
 
-	conn, err := net.DialTimeout("tcp", address, 5*time.Second)
+	_, err := net.DialTimeout("tcp", address, 3 *time.Second)
 	if err != nil {
 		portResult.State = false
 	}
 
-	defer conn.Close()
 	portResult.State = true
 
-	return portResult
+	result <- portResult
 }
